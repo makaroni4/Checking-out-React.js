f0c3a7de16ae3c1923d660e4e12223b5141500e4
Anatoli Makarevich <makaroni4@gmail.com>
2017-02-28 23:05:41 +0100

    I made my first React component mom

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <h1>Hello world!</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App/>, document.getElementsByClassName("app")[0]);
